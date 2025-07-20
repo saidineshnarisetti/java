2d6c71a51e2b3fa5740eee9408335d97e2ea06c7
saidineshnarisetti <dinu.narisetti@gmail.com>
2025-07-20 19:07:00 +0530

    Updated CI/CD flow2-3

--- src/test/java/gradle/junit/selenium/JuiceTest.java
+++ src/test/java/gradle/junit/selenium/JuiceTest.java
@@ -88,7 +88,10 @@ class JuiceTest {
         // Login with credentials
         WebElement emailField = driver.findElement(By.name("email"));
         WebElement passwordField = driver.findElement(By.name("password"));
-        WebElement loginButton = driver.findElement(By.id("loginButton"));
+        //WebElement loginButton = driver.findElement(By.id("loginButton"));
+        WebElement loginButton = new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.elementToBeClickable(By.id("loginButton")));
+        loginButton.click();
 
         emailField.sendKeys(customer.getEmail());
         passwordField.sendKeys(customer.getPassword());
